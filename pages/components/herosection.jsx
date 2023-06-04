1df6bc4420a90821f6ff60715aa5e800aad98b02
Bryan Quiroz <bquiroz125@gmail.com>
2023-06-03 21:27:00 -0500

    hero section change

--- pages/components/herosection.jsx
+++ pages/components/herosection.jsx
@@ -6,7 +6,7 @@ function HeroSection() {
     <section className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white py-20">
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 gap-8">
         <div>
-          <h1 className="text-8xl lg:text-7xl font-bold mb-4">Welcome to the Hero Section</h1>
+          <h1 className="text-6xl lg:text-6xl font-bold mb-4">Welcome to the Hero Section</h1>
           <p className="text-lg sm:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           <button className="bg-white text-blue-500 font-semibold hover:font-bold py-2 px-4 mt-4 rounded shadow">
             Get Started
